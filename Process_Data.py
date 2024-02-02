ee8a431aea6faf0f66c19a435f35f91bf6c219be
Seandowling123 <dowling.sean103@gmail.com>
2024-02-02 12:38:15 +0000

    asdf

--- Process_Data.py
+++ Process_Data.py
@@ -22,3 +22,42 @@ def load_text_file(file_path):
         print(f"An error occurred: {str(e)}")
         return f"An error occurred: {str(e)}"
     
+def process_text(body):
+    try:
+        body_split = ((((body.split("\nBody\n")[1]).split('Load-Date:')[0]).split("\nNotes\n")[0]).replace('\n', '')).replace('  ', '')
+        body_filtered = re.sub(r'[^a-zA-Z ]', '', body_split)
+        return body_filtered
+    except Exception as e:
+        print("Error processing text")
+        return 0, 0
+    
+def convert_string_to_datetime(date_string):
+    try:
+        datetime_object = datetime.strptime(date_string, '%B %d, %Y')
+        return datetime_object
+    except ValueError:
+        return f"Unable to parse the date string: {date_string}"
+    
+def extract_article_data(text_data):
+    articles = []
+    dates = []
+    # Extract data
+    for i in range(len(text_data)):
+        # Extract date
+        date_pattern = re.compile(r'\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\b \d{1,2}, \d{4}')
+        match = date_pattern.search(text_data[i])
+        # Check for valid date
+        if match:
+            date_string = match.group()
+            date = convert_string_to_datetime(date_string)
+            if isinstance(date, datetime):
+                dates.append(date)
+                # Process text body
+                body = process_text(text_data[i])
+                if body != 0:
+                    articles.append(Article(date, body, 0))
+                else: print("Removed article. Incorrect syntax")
+            else: print("Removed article. Date loaded incorrectly")
+        else: print("Removed article. Date not found")
+    print("Loaded", len(articles), "articles")
+    return articles, dates
