678ffe379f04ebd8d877f79fa196571aca4e4261
Seandowling123 <dowling.sean103@gmail.com>
2024-02-03 18:15:34 +0000

    added stuff

--- Get_Curser_Pos.py
+++ Get_Curser_Pos.py
@@ -1,7 +1,4 @@
 import pyautogui
 
 # Display the mouse position
-#pyautogui.displayMousePosition()
-
-px = pyautogui.pixel(100, 500)
-print(px)
+pyautogui.displayMousePosition()
--- testin.py
+++ testin.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import random
 
 time.sleep(4)
 
@@ -14,7 +15,7 @@ pyautogui.click()
 # Search bar
 pyautogui.moveTo(685, 78, duration=1)
 pyautogui.click()
-pyautogui.typewrite("tcd lexisnexis", interval=0.05)
+pyautogui.typewrite("tcd lexisnexis", interval=print(random.randint(.01, .1)))
 pyautogui.press('enter')
 
 # Tcd law website
@@ -27,8 +28,7 @@ pyautogui.scroll(-4000)
 pyautogui.moveTo(819, 637, duration=1.12)
 pyautogui.click()
 
-# If no login page
-time.sleep(15)
+# If login page
 px = pyautogui.pixel(100, 500)
 if px != (0, 24, 46):
     # Tcd email
@@ -51,24 +51,22 @@ pyautogui.click()
 
 # Advanced search
 time.sleep(5)
-pyautogui.moveTo(1511, 595, duration=0.7892347823)
+pyautogui.moveTo(1511, 595, duration=0.7492347823)
 pyautogui.click()
 
 # Search query
 time.sleep(2)
-pyautogui.moveTo(922, 551, duration=0.7892347823)
+pyautogui.moveTo(922, 551, duration=0.7392347823)
 pyautogui.click()
-pyautogui.typewrite("company(Ryanair) and >=1/1/2003 <=31/12/2023 and (publication(Wall Street Journal Abstracts) OR publication(Financial Times Online) OR publication(Associated Press Financial Wire) OR publication(AirGuide Business & AirGuideBusiness.com) OR publication(Proactive Investors (UK)) OR publication(BNS News Service in English by Baltic News Service (BNS) English) OR publication(Newstex Blogs) OR publication(Live Briefs PRO Global Markets) OR publication(MT Newswires Live Briefs) OR publication(Business World (Digest)) OR publication(MarketLine NewsWire) OR publication(London Stock Exchange Regulatory News Service) OR publication(Sunday Business Post) OR publication(International Business Times News) OR publication(The Investors Chronicle) OR publication(Financial Times (London, England)) OR publication(AirFinance Journal) OR publication(Flight International) OR publication(dpa-AFX International ProFeed) OR publication(dpa international (Englischer Dienst)) OR publication(RTT News (United States)) OR publication(Citywire) OR publication(City A.M.) OR publication(ANSA English Corporate Service) OR publication(American Banking and Market News) OR publication(Transcript Daily) OR publication(Watchlist News) OR publication(DailyPolitical) OR publication(Alliance News UK) OR publication(Thomson Financial News Super Focus))", interval=0.001)
+pyautogui.typewrite("company(Ryanair) and >=1/1/2003 <=31/12/2023 and (publication(Wall Street Journal Abstracts) OR publication(Financial Times Online) OR publication(Associated Press Financial Wire) OR publication(AirGuide Business & AirGuideBusiness.com) OR publication(Proactive Investors (UK)) OR publication(BNS News Service in English by Baltic News Service (BNS) English) OR publication(Newstex Blogs) OR publication(Live Briefs PRO Global Markets) OR publication(MT Newswires Live Briefs) OR publication(Business World (Digest)) OR publication(MarketLine NewsWire) OR publication(London Stock Exchange Regulatory News Service) OR publication(Sunday Business Post) OR publication(International Business Times News) OR publication(The Investors Chronicle) OR publication(Financial Times (London, England)) OR publication(AirFinance Journal) OR publication(Flight International) OR publication(dpa-AFX International ProFeed) OR publication(dpa international (Englischer Dienst)) OR publication(RTT News (United States)) OR publication(Citywire) OR publication(City A.M.) OR publication(ANSA English Corporate Service) OR publication(American Banking and Market News) OR publication(Transcript Daily) OR publication(Watchlist News) OR publication(DailyPolitical) OR publication(Alliance News UK) OR publication(Thomson Financial News Super Focus))", interval=random.randint(.001, .005))
 pyautogui.scroll(-4000)
 pyautogui.press('enter')
 
 # Language English
-time.sleep(5)
 pyautogui.moveTo(303, 594, duration=0.7892347823)
 pyautogui.click()
 
 # Search
-time.sleep(5)
 pyautogui.moveTo(359, 684, duration=0.7892347823)
 pyautogui.click()
 
