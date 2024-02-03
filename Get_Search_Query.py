90d3b140c4ce4b9124e1637ca7850bfb883799b2
Seandowling123 <dowling.sean103@gmail.com>
2024-02-03 18:02:25 +0000

    stuff

--- Get_Search_Query.py
+++ Get_Search_Query.py
@@ -5,7 +5,7 @@ def read_csv_and_print(filename):
         reader = csv.reader(file)
         next(reader)  # Skip the header row
         for row in reader:
-            print(f"publication({row[0]}) OR ", end='')
+            print(f"publication({row[1]}) OR ", end='')
 
 # Assuming the csv file is named News_Source_Names.csv
 csv_filename = 'News_Source_Names.csv'
