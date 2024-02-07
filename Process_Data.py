dafeabac2d72dad6b38ce62f59377729cc34b549
Seandowling123 <dowling.sean103@gmail.com>
2024-02-07 13:03:34 +0000

    adding other finance variables

--- Process_Data.py
+++ Process_Data.py
@@ -13,17 +13,19 @@ nltk.download('punkt')
 
 # Data to save for each trading day
 class Trading_Day:
-    def __init__(self, date, close, returns, absolute_returns, monday, january, sentiment):
+    def __init__(self, date, close, returns, absolute_returns, volume, vix, monday, january, sentiment):
         self.date = date
         self.close = close
         self.returns = returns
         self.absolute_returns = absolute_returns
+        self.volume = volume
+        self.vix = vix
         self.monday = monday
         self.january = january
         self.sentiment = sentiment
     
     def to_csv_line(self):
-        return f"{str(self.date)},{str(self.close)},{str(self.returns)},{str(self.absolute_returns)},{str(self.monday)},{str(self.january)},{str(self.sentiment)}"
+        return f"{str(self.date)},{str(self.close)},{str(self.returns)},{str(self.absolute_returns)},{str(self.volume)},{str(self.vix)},{str(self.monday)},{str(self.january)},{str(self.sentiment)}"
 
 # Class containing info about each article
 class Article:
@@ -150,31 +152,72 @@ def get_sentiment_scores(articles, positive_dict, negative_dict):
             
         except Exception as e:
             print(f"An sentiment calculation error occurred: {str(e)}")
+            
+# Compute log of each value in a list   
+def get_logs(input_list):
+    logs = []
+    for num in input_list:
+        logs.append(math.log(num))
+    return logs
 
 # Extract financial data 
-def ectract_close_prices(file_path, start_date, end_date):
-    filtered_data_dict = {}
+def get_RYAAY_data(file_path, start_date, end_date):
+    close_price_dict = {}
+    trading_vol_dict = {}
+    volume = []
     range_reached = 0
+    index = 0
     try:
         with open(file_path, 'r', newline='') as input_file:
             reader = csv.DictReader(input_file)
-            print(start_date)
+            
             for row in reader:
                 date_str = row['Date']
                 date_object = datetime.strptime(date_str, '%Y-%m-%d')
                 close_price = float(row['Adj Close'])
+                volume.append(row['Volume'])
 
                 if start_date <= date_object <= end_date:
                     # Add the date before range for returns calculation
                     if range_reached == 0:
-                        filtered_data_dict[prev_date] = prev_close
+                        close_price_dict[prev_date] = prev_close
                         range_reached = 1
-                    filtered_data_dict[date_object] = close_price
+                    close_price_dict[date_object] = close_price
+                    
+                    # Get detrended trading volume
+                    #detrended_vol = np.mean(get_logs(volume[index-60:index]))
+                    detrended_vol = volume[index]
+                    trading_vol_dict[date_object] = detrended_vol
                     
                 prev_date = date_object
                 prev_close = close_price
+                index = index+1
 
-        return filtered_data_dict
+        return close_price_dict, trading_vol_dict
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+    
+def get_VIX_data(file_path, start_date, end_date):
+    close_price_dict = {}
+    try:
+        with open(file_path, 'r', newline='') as input_file:
+            reader = csv.DictReader(input_file)
+            
+            for row in reader:
+                date_str = row['Date']
+                if row['Adj Close'] != "null":
+                    date_object = datetime.strptime(date_str, '%Y-%m-%d')
+                    close_price = float(row['Adj Close'])
+
+                    if start_date <= date_object <= end_date:
+                        close_price_dict[date_object] = close_price
+                else: close_price_dict[date_object] = 0
+
+        return close_price_dict
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         return None
@@ -195,21 +238,23 @@ def is_january(date):
     else: return 0
 
 # Collect data for each trading day
-def get_trading_day_data(daily_senitment, close_prices):
+def get_trading_day_data(daily_senitment, close_prices, trading_volume, VIX_prices):
     trading_days = {}
     prev_date = 0
     for date in close_prices:
         if prev_date != 0:
-            # Get data
+            # Compile data
             close = close_prices[date]
             returns = math.log(close_prices[date]/close_prices[prev_date])
             monday = is_monday(date)
             january = is_january(date)
+            volume = trading_volume[date]
+            vix = VIX_prices[date]
             if date in daily_senitment:
                 senitment = daily_senitment[date]
             else: senitment = 0
             # Store in trading days dict
-            trading_days[date] = Trading_Day(date, close, returns, abs(returns), monday, january, senitment)
+            trading_days[date] = Trading_Day(date, close, returns, abs(returns), volume, vix, monday, january, senitment)
         prev_date = date
     return trading_days
 
@@ -218,7 +263,7 @@ def save_trading_days_to_csv(trading_days, csv_file_path):
         with open(csv_file_path, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             # Header
-            writer.writerow(["Date", "Close", "Returns", "AbsoluteReturns", "Monday", "January", "Sentiment"])
+            writer.writerow(["Date", "Close", "Returns", "Absolute_Returns", "Detrended_Volume", "VIX", "Monday", "January", "Sentiment"])
             # Save data
             for date, trading_day in trading_days.items():
                 writer.writerow(trading_day.to_csv_line().split(','))
@@ -264,9 +309,10 @@ for article in articles:
 # Extract financial data from the time period
 start_date = min(dates)
 end_date = max(dates)
-close_prices = ectract_close_prices("RYAAY.csv", start_date, end_date)#
-
-trading_days = get_trading_day_data(daily_senitment, close_prices)
+close_prices, trading_volume = get_RYAAY_data("RYAAY.csv", start_date, end_date)
+VIX_prices = get_VIX_data("VIX.csv", start_date, end_date)
+print(VIX_prices)
+trading_days = get_trading_day_data(daily_senitment, close_prices, trading_volume, VIX_prices)
 
 # Save trading day data to csv
 csv_file_path = 'trading_days_data.csv'
@@ -277,9 +323,11 @@ dates = list(trading_days.keys())
 sentiments = [trading_days[date].sentiment for date in trading_days]
 closes = [trading_days[date].close for date in trading_days]
 returns = [trading_days[date].returns for date in trading_days]
+volume = [trading_days[date].volume for date in trading_days]
+vix = [trading_days[date].vix for date in trading_days]
 
 # Creating line plot
-plt.plot(dates, returns, color='red', label='Returns')
+plt.plot(dates, volume, color='red', label='Returns')
 plt.plot(dates, sentiments, label='Sentiment')
 plt.title('Values Over Time')
 plt.xlabel('Date')
@@ -309,5 +357,4 @@ if mode == "test":
     print(f"Sentiment: {articles[0].sentiment}\n")
     
     print(daily_senitment)
-    print(trading_days)
 
