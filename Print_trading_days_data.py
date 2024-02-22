e34c50302ec2ec6d18496c25af44afb861d2b936
Seandowling123 <dowling.sean103@gmail.com>
2024-02-22 13:30:07 +0000

    run again

--- Print_trading_days_data.py
+++ Print_trading_days_data.py
@@ -3,19 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data
-df = pd.read_csv('trading_days_data.csv')
+df = pd.read_csv('XX_daily_data.csv')
 df.set_index('Date', inplace=True)
 
 # Select relevant columns for modeling
-df = df[['Returns', 'Sentiment']]
-
-df = pd.read_csv('trading_days_data.csv')
-print(list(df["Date"]))
+df = df[['Returns', 'Positive_Sentiment']]
 
 # Plot data
 plt.figure(figsize=(10, 6))
 plt.plot(df.index, df['Returns'], label='Actual Returns')
-plt.plot(df.index, df['Sentiment'], label='Sentiment')
+plt.plot(df.index, df['Positive_Sentiment'], label='Sentiment')
 plt.title('VAR Forecasting with Training and Testing Data')
 plt.xlabel('Date')
 plt.ylabel('Returns')
