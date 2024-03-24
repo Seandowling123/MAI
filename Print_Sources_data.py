766dc400390d734aa420816e9914cdad31e18c4f
Seandowling123 <dowling.sean103@gmail.com>
2024-03-24 14:55:55 +0000

    format sources date

--- Print_Sources_data.py
+++ Print_Sources_data.py
@@ -1,4 +1,6 @@
+import re
 import csv
+from datetime import datetime
 
 # Load dictionary words from csv
 def load_csv(file_path):
@@ -11,17 +13,36 @@ def load_csv(file_path):
         return f"File not found: {file_path}"
     except Exception as e:
         return f"An error occurred: {str(e)}"
+    
+def format_date(field):
+    pattern = r"From (\w+ \d{4}) through current"
+    match = re.search(pattern, field)
+
+    if match:
+        date_str = match.group(1)
+        date_obj = datetime.strptime(date_str, "%B %Y")
+        formatted_date_str = date_obj.strftime("%d/%m/%Y")
+    else: print(" No date match")
+    return formatted_date_str
+
 
 
 file_path = 'Sources_Data.csv'                
 data = load_csv(file_path)
 
-for row in data:
+for row in data[2:]:
     print(f"\\textbf{{{row[0]}}} ", end='', flush=True)
-    for field in row[1:]:
-        print(f" & {field}", end='', flush=True)
+    for field in row[1:len(row)-2]:
+        if field != row[1]:
+            if field == row[4]:
+                date = field.split(' -')[0]
+                date = datetime.strptime(date, "%B %d, %Y")
+                date = date.strftime("%d/%m/%Y")
+                
+                print(f" & {date} -{field.split(' -')[1]}", end='', flush=True)
+            else:
+                print(f" & {field}", end='', flush=True)
     print('\\\\')
-#print(f"\\textbf{{{source_name}}} & {count}\\\\")
 
 for row in data:
     print(f"\\textbf{{{row[0]}}} ", end='', flush=True)
