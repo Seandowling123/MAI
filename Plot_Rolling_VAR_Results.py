8150bcfbdc64f28aff036de669f808f78f4a0226
Seandowling123 <dowling.sean103@gmail.com>
2024-04-04 14:36:46 +0100

    done for neg and count for returns

--- Plot_Rolling_VAR_Results.py
+++ Plot_Rolling_VAR_Results.py
@@ -13,26 +13,83 @@ df.set_index('obs', inplace=True)
 dates = df.index.tolist()
 datetime_objs = [datetime.strptime(date_str.split(' ')[0], '%Y-%m-%d') for date_str in dates]
 
+# Calculate the negative sentiment significance levels 
+significance_levels = [[] for _ in range(5)]
+for i, p_value_series in enumerate((df['neg_p_values_lag_one'], df['neg_p_values_lag_two'], df['neg_p_values_lag_three'], df['neg_p_values_lag_four'],df['neg_p_values_lag_five'])):
+    for p_value in p_value_series:
+        if p_value <= 0.01:
+            significance_levels[i].append(3)
+        elif p_value <= 0.05:
+            significance_levels[i].append(2)
+        elif p_value <= 0.1:
+            significance_levels[i].append(1)
+        else:
+            significance_levels[i].append(0)
+
 # Plot negative sentiment significance
-fig, ax = plt.subplots(figsize=(15, 2))
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(16, 14), sharex=False, sharey=True)
 
 # Plot each line plot on the same axis
-ax.plot(datetime_objs, df['neg_p_values_lag_one'], label='Negative Sentiment Lag-1 Statistical Significance', color='#2980b9', linewidth=1)
-ax.plot(datetime_objs, df['neg_p_values_lag_two'], label='Negative Sentiment Lag-2 Statistical Significance', color='#2980b9', linewidth=1)
-ax.plot(datetime_objs, df['neg_p_values_lag_three'], label='Negative Sentiment Lag-3 Statistical Significance', color='#2980b9', linewidth=1)
-ax.plot(datetime_objs, df['neg_p_values_lag_four'], label='Negative Sentiment Lag-4 Statistical Significance', color='#2980b9', linewidth=1)
-ax.plot(datetime_objs, df['neg_p_values_lag_five'], label='Negative Sentiment Lag-5  Statistical Significance', color='#2980b9', linewidth=1)
+ax1.plot(datetime_objs, significance_levels[0], label='Negative Sentiment Lag-1 Statistical Significance', color='#2980b9', linewidth=1)
+ax2.plot(datetime_objs, significance_levels[1], label='Negative Sentiment Lag-2 Statistical Significance', color='#2980b9', linewidth=1)
+ax3.plot(datetime_objs, significance_levels[2], label='Negative Sentiment Lag-3 Statistical Significance', color='#2980b9', linewidth=1)
+ax4.plot(datetime_objs, significance_levels[3], label='Negative Sentiment Lag-4 Statistical Significance', color='#2980b9', linewidth=1)
+ax5.plot(datetime_objs, significance_levels[4], label='Negative Sentiment Lag-5  Statistical Significance', color='#2980b9', linewidth=1)
 
-ax.set_title('Statistical Significance of Negative Sentiment at Different Lags on Returns Over Time', fontsize=14, fontfamily='serif')
-ax.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-ax.legend(fontsize=11, loc='upper left', prop={'family': 'serif', 'size': 11})
-ax.xlabel('Date', fontsize=11, fontname='Times New Roman')
-ax.ylabel('Statistical Significance Level', fontsize=11, fontname='Times New Roman')
-ax.yticks([1, 2, 3], ['10%', '5%', '1%'])
-ax.grid(axis='y', linestyle='--', alpha=0.7)
-ax.tick_params(axis='both', which='major', labelsize=10)
-ax.savefig('Plots/Returns_Negative_Sentiment_Significance.png', bbox_inches='tight')
+# Plot settings
+for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+    ax.set_title(f'Statistical Significance of Negative Sentiment at Lag-{i+1} on Returns Over Time', fontsize=14, fontfamily='serif')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    ax.legend(fontsize=11, loc='upper left', prop={'family': 'serif', 'size': 11})
+    ax.set_xlabel('Date', fontsize=11, fontname='Times New Roman')
+    ax.set_ylabel('Statistical Significance Level', fontsize=11, fontname='Times New Roman')
+    ax.set_yticks([1, 2, 3], ['10%', '5%', '1%'])
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    ax.tick_params(axis='both', which='major', labelsize=10)
+
+plt.subplots_adjust(hspace=.5)
+plt.savefig('Plots/Returns_Negative_Sentiment_Significance.png', bbox_inches='tight')
 #plt.show()
+
+
+# Calculate the article count significance levels 
+significance_levels = [[] for _ in range(5)]
+for i, p_value_series in enumerate((df['count_p_values_lag_one'], df['count_p_values_lag_two'], df['count_p_values_lag_three'], df['count_p_values_lag_four'],df['count_p_values_lag_five'])):
+    for p_value in p_value_series:
+        if p_value <= 0.01:
+            significance_levels[i].append(3)
+        elif p_value <= 0.05:
+            significance_levels[i].append(2)
+        elif p_value <= 0.1:
+            significance_levels[i].append(1)
+        else:
+            significance_levels[i].append(0)
+
+# Plot article count significance
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(16, 14), sharex=False, sharey=True)
+
+# Plot each line plot on the same axis
+ax1.plot(datetime_objs, significance_levels[0], label='Article Count Lag-1 Statistical Significance', color='#2980b9', linewidth=1)
+ax2.plot(datetime_objs, significance_levels[1], label='Article Count Lag-2 Statistical Significance', color='#2980b9', linewidth=1)
+ax3.plot(datetime_objs, significance_levels[2], label='Article Count Lag-3 Statistical Significance', color='#2980b9', linewidth=1)
+ax4.plot(datetime_objs, significance_levels[3], label='Article Count Lag-4 Statistical Significance', color='#2980b9', linewidth=1)
+ax5.plot(datetime_objs, significance_levels[4], label='Article Count Lag-5  Statistical Significance', color='#2980b9', linewidth=1)
+
+# Plot settings
+for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+    ax.set_title(f'Statistical Significance of Article Count at Lag-{i+1} on Returns Over Time', fontsize=14, fontfamily='serif')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    ax.legend(fontsize=11, loc='upper left', prop={'family': 'serif', 'size': 11})
+    ax.set_xlabel('Date', fontsize=11, fontname='Times New Roman')
+    ax.set_ylabel('Statistical Significance Level', fontsize=11, fontname='Times New Roman')
+    ax.set_yticks([1, 2, 3], ['10%', '5%', '1%'])
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    ax.tick_params(axis='both', which='major', labelsize=10)
+
+plt.subplots_adjust(hspace=.5)
+plt.savefig('Plots/Returns_Article_Count_Significance.png', bbox_inches='tight')
+#plt.show()
+
 
 # Plot data
 plt.figure(figsize=(15, 2))
